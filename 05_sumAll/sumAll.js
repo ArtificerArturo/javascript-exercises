d0ab6eb2c83329c28f37e463dcb8327d6bf03b33
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-03 14:24:29 -0600

    solution to 05 found

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return "ERROR"
+    if (startNum < 0 || endNum < 0) return "ERROR"
+
+    let distanceBetween = Math.abs(startNum - endNum)
+    let answer = 0
+    if (startNum <= endNum) {
+        for (i = 0; i <= distanceBetween; i++) {
+            answer += startNum + i
+        }
+    } else {
+        for (i = 0; i <= distanceBetween; i++) {
+            answer += endNum + i
+        }
+    }
+    return answer
 
 };
+console.log(sumAll(5, 12))
+console.log(sumAll(12, 5))
 
 // Do not edit below this line
 module.exports = sumAll;
