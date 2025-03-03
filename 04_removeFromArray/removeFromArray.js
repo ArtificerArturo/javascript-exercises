fa9edf4d5a10f07e973c8c26f066d36e9ac4548a
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-03 13:31:18 -0600

    solution to 04 found

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayToBePruned, ...valueToPrune) {
+
+    for (let individualValueToPrune of valueToPrune) {
+
+        let indexOfBadValue = arrayToBePruned.indexOf(individualValueToPrune)
+        while (arrayToBePruned.indexOf(individualValueToPrune) > -1)
+            arrayToBePruned.splice(indexOfBadValue, 1)
+
+    }
+
+    return arrayToBePruned
 };
+
+//console.log(removeFromArray([5, 6, 7, 6, 8, 0], 6, 7))
 
 // Do not edit below this line
 module.exports = removeFromArray;
