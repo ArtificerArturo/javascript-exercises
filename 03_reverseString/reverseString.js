aa8e17fbbc62cf526db6d10335a7216b8bc7c249
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-03 12:36:58 -0600

    solution found

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function (text) {
+    let letterArray = text.split("")
+    let arrayLength = letterArray.length
+    let reversedArray = []
+    for (i = arrayLength - 1; i > -1; i--) {
+        reversedArray.push(letterArray[i])
+    }
+    let reversedString = reversedArray.join("")
+    return reversedString
 
 };
+
+console.log(reverseString("nascar"))
 
 // Do not edit below this line
 module.exports = reverseString;
